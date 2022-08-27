530b100e91e12a4500d488a0f5af534d37f11e71
Sadhna kumari <sadhnakhairia1998@gmail.com>
2022-08-27 21:38:30 +0530

    countdown

---
+++ Countdown/script.js
@@ -0,0 +1,39 @@
+const finalDate = new Date ("Dec 20, 2022 00:00:00").getTime();
+// console.log(finalDate)
+
+// get time will return the number of mili seconds
+
+// ----------------------creating timer function-----------------------
+
+timer = () =>{
+    const now = new Date().getTime();
+    const diff = finalDate - now;
+
+    if(diff <0){
+        clearInterval(x);
+        document.getElementById('container').style.display = "none";
+        document.getElementById('if-expired').style.display = "block";
+
+    }
+
+
+    let days = Math.floor(diff/(1000*60*60*24));
+    let hours = Math.floor((diff % (1000*60*60*24)) / (1000*60*60));
+    let minutes = Math.floor((diff % (1000*60*60)) / (1000*60));
+    let seconds = Math.floor((diff%(1000*60)) / 1000);
+
+    days<=9 ? days = `00${days}`:
+    days<=99 ? days = `0${days}`: days;
+    hours<=9 ? hours = `0${hours}`: hours;
+    minutes<=9 ? days = `0${minutes}`:minutes;
+    seconds<=9 ? seconds = `0${seconds}`: seconds;
+
+
+    document.querySelector("#days .box").textContent = days;
+    document.querySelector("#hours .box").textContent = hours;
+    document.querySelector("#minutes .box").textContent = minutes;
+    document.querySelector("#seconds .box").textContent = seconds;
+
+}
+
+const x = setInterval(timer, 1000); 
